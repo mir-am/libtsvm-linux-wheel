1ed05592214642e69ca95ba6051f573cdfd25d3e
Mir, A <mir-am@hotmail.com>
2019-05-27 20:11:47 +0430

    Uploading to Dropbox.

--- upload.py
+++ upload.py
@@ -6,25 +6,19 @@ Created on Sat May 25 10:36:35 2019
 """
 
 
-import ftplib
+import dropbox
 import glob
 import os
 import sys
 
-sev_addr = 'ftp.mirblog.ir'
-usr = 'libtsvm@mirblog.ir'
-pwd = '$@!7324Hm'
+acc_token = 'Aq4crTcY2oAAAAAAAAAAC9i5oxSy7n-Hy9wUBADbeg7oNxOHKsyO8C1tx_H21dPn'
+file_to = '/wheels/'
 whl_path = sys.argv[1]
-time_out = 300
 
-session = ftplib.FTP(sev_addr, usr, pwd, timeout=time_out)
-session.set_pasv(False)
+dbx = dropbox.Dropbox(acc_token)
 
 os.chdir(whl_path)
 for whl_file in glob.glob('*.whl'):
     
-    file = open(whl_file, 'rb')
-    session.storbinary('STOR %s' % whl_file, file)
-
-file.close()
-session.quit()
+    with open(whl_file, 'rb') as f:
+        dbx.files_upload(f.read(), file_to + whl_file)
